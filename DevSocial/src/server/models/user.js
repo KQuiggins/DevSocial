1a2fc52383203f7ea9aa5ceb7867b464b95dad69
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2024-02-08 14:09:01 -0500

    Refactor user model import statement

--- DevSocial/src/server/models/user.js
+++ DevSocial/src/server/models/user.js
@@ -1,8 +1,6 @@
+import { Model } from 'sequelize';
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
   class User extends Model {
     /**
      * Helper method for defining associations.
