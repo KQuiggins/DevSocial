7a9096a216cf78fa4279ae91d9d9a1ebe36cc670
Kenneth Quiggins <kashfamilyvalues@gmail.com>
2024-03-26 19:12:34 -0400

    Refactor Chats component and add lastMessage field to Chat type

--- DevSocial/src/client/components/Chats.tsx
+++ DevSocial/src/client/components/Chats.tsx
@@ -1,22 +1,23 @@
-import { Container, Row, Col, Image, ListGroup } from 'react-bootstrap';
+import { Container, Row, Image } from 'react-bootstrap';
+import { gql, useQuery } from '@apollo/client';
 
-const chats = [
+const GET_CHATS = gql`
 	{
-		id: 2,
-		users: [
-			{
-				avatar: '/uploads/mafia.jpg',
-				username: 'TestUser',
-				id: 3,
-			},
-			{
-				avatar: '/uploads/woman.jpg',
-				username: 'TestUser2',
-				id: 4,
-			},
-		],
-	},
-];
+		chats {
+			id
+			users {
+				id
+				avatar
+				username
+			}
+			lastMessage {
+				text
+			}
+		}
+	}
+`;
+
+
 
 const usernamesToString = (users) => {
 	if (users.length <= 1) return '';
@@ -29,40 +30,55 @@ const shorten = (text) => {
 };
 
 const Chats = () => {
+	const { loading, error, data } = useQuery(GET_CHATS);
+	if (loading)
+		return (
+			<div className='chats'>
+				<p>Loading...</p>
+			</div>
+		);
+	if (error)
+		return (
+			<div className='chats'>
+				<p>{error.message}</p>
+			</div>
+		);
+	const { chats } = data;
 	return (
 		<Container
-      fluid
-      className='p-0 chats'
-      style={{
-        height: '50vh', // This line adjusts the height to 50% of the viewport height
-        width: '200px',
-        backgroundColor: '#eee',
-        borderLeft: '1px solid #c3c3c3',
-        overflowY: 'scroll' // Add scroll for overflow
-      }}
-    >
-      {chats.map((chat) => (
-        <Row
-          key={chat.id}
-          className='chat'
-          noGutters
-          style={{ cursor: 'pointer' }}
-        >
-          <Image
-            src={
-              chat.users.length > 2
-                ? '/public/group.png'
-                : chat.users[1].avatar
-            }
-            roundedCircle
-            style={{ width: '50px', margin: '5px' }}
-          />
-          <div style={{ marginLeft: '5px' }}>
-            <h2>{shorten(usernamesToString(chat.users))}</h2>
-          </div>
-        </Row>
-      ))}
-    </Container>
+			fluid
+			className='p-0 chats'
+			style={{
+				height: '50vh', // This line adjusts the height to 50% of the viewport height
+				width: '200px',
+				backgroundColor: '#eee',
+				borderLeft: '1px solid #c3c3c3',
+				overflowY: 'scroll', // Add scroll for overflow
+			}}
+		>
+			{chats.map((chat) => (
+				<Row
+					key={chat.id}
+					className='chat'
+					noGutters
+					style={{ cursor: 'pointer' }}
+				>
+					<Image
+						src={
+							chat.users.length > 2
+								? '/public/group.png'
+								: chat.users[1].avatar
+						}
+						roundedCircle
+						style={{ width: '50px', margin: '5px' }}
+					/>
+					<div style={{ marginLeft: '5px' }}>
+						<h2>{shorten(usernamesToString(chat.users))}</h2>
+						<span>{chat?.lastMessage?.text}</span>
+					</div>
+				</Row>
+			))}
+		</Container>
 	);
 };
 
